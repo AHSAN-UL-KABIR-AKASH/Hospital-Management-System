6ae9291bac1ef59fb402417a5bf0faf9f0f8254a
turja007-rgb <agradipmodokturja@gmail.com>
2025-07-23 21:54:45 +0600

    project added

---
+++ demo/from.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+    <title>Mid_Task_2.png</title>
+
+</head>
+<body>
+
+<style>
+    body {background-color:lightblue;}
+    h1,h2{color:yelow;}
+    table
+    {
+
+        background-color: #fff;
+        border: 1px solid: #ccc;
+        margin: auto;
+        padding: 20px;
+    }
+    
+    input[type="submit"] {
+        background-color: skyblue;
+        color: red;
+        padding: 20px;
+        cursor: pointer;
+    }
+    </style>
+
+    <center>
+        <h1 style="color:blue;">Bank Management System</h1>
+        <h2 style="color:green;">Your trusted financial partner</h2>
+        <h4 style="colour:blue">Customer Registation Form</h4>
+    </center>
+    <table>
+        <tr>
+            <td>Full Name:</td><td><input type="text"></td>
+        </tr>
+
+        <tr>
+            <td>Date of Birth:</td><td><input type="date"></td>
+        </tr>
+
+        <tr>
+            <td>Enter Your Email:</td><td><input type="text"></td>
+        </tr>
+
+        <tr>
+            <td>Enter Your Password:</td><td><input type="text"></td>
+        </tr>
+
+        <tr>
+            <td>
+                Gender:</td>
+            <td><input type="radio" name="des">Male</td>
+            <td><input type="radio" name="des">Female</td>
+            <td><input type="radio" name="des">other</td>
+        </tr> 
+
+        <tr>
+            <td>Maritial status</td>
+            <td>
+                <select Name="">
+                    <option value="single">Single</option>
+                    <option value="married">Married</option>
+                    <option value="divorced">Divorced</option>
+                    </select>
+            </td>
+
+
+            <tr>
+                <td>Account Type</td>
+                <td>
+                    <select Name="">
+                        <option value="saving">Saving</option>
+                        <option value="current">Current</option>
+                        <option value="fixed">Fixed</option>
+                    </select>
+                </td>
+
+
+                <tr>
+                    <td> Initial Deposit Ammount:</td>
+                    <td><input type="text"></td>
+                </tr>
+
+                <tr>
+                    <td>Phone Number:</td>
+                    <td><input type="text"></td>
+                </tr>
+
+                <tr>
+                    <td>Email Address:</td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td>Address:</td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td>Occupation</td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td>National ID:</td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td>Upload ID Proof:</td>
+                    <td><input type="file"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="checkbox"> I agree to the terms and conditions
+                    </td>
+                </tr>
+
+                </tr> <br>
+
+                 <br>
+                <tr>
+                    <td>
+                        <input type="submit" value="Register" style="color:red;">
+                        <input type="submit" value="Clear" style="color:black;">
+                    </td>
+                </tr>
+            </table>
+            <br>
+    
+</body>
+</html>
